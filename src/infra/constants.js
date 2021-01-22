fdcfb61d056c22be62f165e42b4d3826e98c2ad7
Jin <david.jeong0611@gmail.com>
2021-01-22 15:22:24 +0900

    Update constants.js

--- src/infra/constants.js
+++ src/infra/constants.js
@@ -1,4 +1,4 @@
 export const SERVER_BASE_URL =
   process.env.REACT_APP_MODE === 'dev'
     ? 'http://127.0.0.1:8000/api/v1'
-    : 'https://dev-dot-landproject-300105.du.r.appspot.com/api/v1';
+    : 'http://land-liveplus-backend-dev.ap-northeast-2.elasticbeanstalk.com/api/v1';
