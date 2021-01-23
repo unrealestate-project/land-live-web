b77eb5b8346653e78349ba3d0f3b8b354923b805
Jin <david.jeong0611@gmail.com>
2021-01-23 23:09:50 +0900

    Update styles.js

--- src/common/Container/styles.js
+++ src/common/Container/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -6,9 +6,9 @@ export const Container = styled.div`
   max-width: 1280px;
   margin-right: auto;
   margin-left: auto;
-  padding: ${(props) => (props.padding ? "2.8rem" : "0 25px")};
+  padding: ${(props) => (props.padding ? '2.8rem' : '0 25px')};
   overflow: hidden;
-  border-top: ${(props) => (props.border ? "1px solid #CDD1D4" : "")};
+  border-top: ${(props) => (props.border ? '1px solid #CDD1D4' : '')};
 
   @media only screen and (max-width: 1024px) {
     max-width: 950px;
@@ -16,6 +16,9 @@ export const Container = styled.div`
 
   @media only screen and (max-width: 768px) {
     max-width: 700px;
+    .eovOeu {
+      padding: 0rem;
+    }
   }
 
   @media only screen and (max-width: 414px) {
